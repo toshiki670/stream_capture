ab3fc98e75b8a36b91cf425f6f5727c0adf1b357
Toshiki <toshiki.dev@protonmail.ch>
2020-06-11 06:56:29 +0900

    Add interface 'feature/#2'

--- lib/stream_capture.rb
+++ lib/stream_capture.rb
@@ -5,5 +5,30 @@ require 'stream_capture/version'
 
 module StreamCapture
   class NoBlockExistsError < StandardError; end
-  # Your code goes here...
+
+  def capture_stdout(&block)
+    StreamCapture.stdout(&block)
+  end
+
+  def capture_stderr(&block)
+    StreamCapture.stderr(&block)
+  end
+
+  def capture_std_both(&block)
+    StreamCapture.std_both(&block)
+  end
+
+  class << self
+    def stdout(&block)
+      StreamCapture::Core.new.stdout(&block)
+    end
+
+    def stderr(&block)
+      StreamCapture::Core.new.stderr(&block)
+    end
+
+    def std_both(&block)
+      StreamCapture::Core.new.std_both(&block)
+    end
+  end
 end
