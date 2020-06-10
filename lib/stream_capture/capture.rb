68b01296758763b3bf0d4a7c722b384c02f5ac99
Toshiki <toshiki.dev@protonmail.ch>
2020-06-11 04:57:56 +0900

    Add capture both 'feature/#2'

--- lib/stream_capture/capture.rb
+++ lib/stream_capture/capture.rb
@@ -12,14 +12,21 @@ module StreamCapture
       capture(:stderr, &block)
     end
 
+    def std_both(&block)
+      string_io = StringIO.new
+      capture(:stdout, string_io) do
+        capture(:stderr, string_io, &block)
+      end
+    end
+
     private
 
     # rubocop:disable Security/Eval, Metrics/MethodLength
-    def capture(stream)
+    def capture(stream, string_io = StringIO.new)
       raise StreamCapture::NoBlockExistsError unless block_given?
 
       stream = stream.to_s
-      binding.eval("$#{stream} = StringIO.new", __FILE__, __LINE__)
+      binding.eval("$#{stream} = string_io", __FILE__, __LINE__)
       begin
         yield
       rescue SystemExit => e
